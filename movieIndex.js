efb14996663eab2c1a5dfbed7056abd12ece0b02
[Shann Roberts] <[dpr.1979@bigpond.com]>
2022-12-05 14:12:11 +0800

    redo package.json from CLI

---
+++ movieIndex.js
@@ -0,0 +1,18 @@
+const movies = [
+    {
+        "title": "Lightyear",
+        "genre": "Sci Fi Adventure",
+        "style": "Animation",
+        "rating": "PG",
+        "storyline": "Buzz Light Year origin story - space may not be the only frontier Buzz needs to conquer",
+        "releaseYear": "2022"
+    },
+    {
+        "title": "Mitchells verses the Machines",
+        "genre": "Sci Fi Adventure",
+        "style": "Animation",
+        "rating": "PG",
+        "storyline": "Buzz Light Year origin story - space may not be the only frontier Buzz needs to conquer",
+        "releaseYear": "2022"
+    }
+]
