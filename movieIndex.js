c37c07b254c47aa4476fc605fd5084f9a6f3f382
[Shann Roberts] <[dpr.1979@bigpond.com]>
2022-12-05 14:23:27 +0800

    create movies array

--- movieIndex.js
+++ movieIndex.js
@@ -8,11 +8,19 @@ const movies = [
         "releaseYear": "2022"
     },
     {
-        "title": "Mitchells verses the Machines",
+        "title": "Mitchells vs. the Machines",
         "genre": "Sci Fi Adventure",
         "style": "Animation",
         "rating": "PG",
-        "storyline": "Buzz Light Year origin story - space may not be the only frontier Buzz needs to conquer",
-        "releaseYear": "2022"
+        "storyline": "Join Katie who just wants to go to film school, but ends up on a road trip with her family... and saving the world from robot domination",
+        "releaseYear": "2021"
+    }
+    {
+        "title": "Raya and the Last Dragon",
+        "genre": "Fantasy Adventure",
+        "style": "Animation",
+        "rating": "PG",
+        "storyline": "Raya is a warrior princess determined to save her world from a plague turning everyone to stone - but first she has to find the Last Dragon",
+        "releaseYear": "2021"
     }
 ]
