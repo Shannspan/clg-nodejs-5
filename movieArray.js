a5fa0ff3f97cfb40080acc702c4646f47f158f22
[Shann Roberts] <[dpr.1979@bigpond.com]>
2022-12-06 13:35:39 +0800

    fix required on push to array

--- movieArray.js
+++ movieArray.js
@@ -1,54 +1,85 @@
-// Iterate through movies list with setTimeout of 1 second enclosing
+// // Iterate through movies list with setTimeout of 1 second enclosing
 
 const forwardFilmList = [{"Lightyear": "Sci fi Action"}, {"Brave": "Historical drama"}, {"Moana": "Adventure"}];    
 
-async function getMovies() {
-    return forwardFilmList.reverse();
+// async function getMovies() {
+//     return forwardFilmList.reverse();
+// }
 
-} 
-// check reverse method is working
-//console.log(getMovies);
+// // check reverse method is working
+// //console.log(getMovies);
 
-function movieTimer() {
-    return new Promise (resolve => {
-    setTimeout(() => {
-    resolve(getMovies());
-    }, 1000);
-})
-}
+// function movieTimer() {
+//     return new Promise (resolve => {
+//     setTimeout(() => {
+//     resolve(getMovies());
+//     }, 1000);
+// })
+// }
 
-// // async / await function
-async function callMovies() {    
-    console.log('Waiting');
-    const oneSecWait = await movieTimer();
-    console.log(oneSecWait);
-}
-callMovies();
+// // // async / await function
+// async function callMovies() {    
+//     console.log('Waiting');
+//     const oneSecWait = await movieTimer();
+//     console.log(oneSecWait);
+// }
+// callMovies();
+
 
 // returns 'Waiting'
 // then after 1 second
 // array in reverse
 
-//FIX REQUIRED: 1000MS TIMER IS WORKING BUT PRODUCES createMovies
 
-async function createMovies() {
-    return forwardFilmList.push({Mulan: 'Historical Action'});    
-} 
+//FIX REQUIRED FOR SECOND SET OF FUNCTIONS AS PUSH NOT APPEARING TO WORK: 
+//$ node movieArray.js 
+// Four animated films
+// 4
+// 4
+// New movie coming
 
-function movieTimer2() {
-    return new Promise (resolve => {
+
+let newFilmList = forwardFilmList.push({Mulan: 'Historical Action'});    
+
+
+
+
+function createMovies() {
+
+    return new Promise (resolve => {        
     setTimeout(() => {
-    resolve(createMovies());
+    resolve ("New movie coming");
+    console.log(newFilmList);
+    
     }, 2000);
-})
+});
 }
+
+createMovies();
 
 async function callMovies2() {       
-    console.log('New movie coming');
-    const twoSecNew = await Promise.all(movieTimer2((async() => {
-    return await createMovies();   
-})));
-console.log(twoSecNew);
-}
-createMovies();
+    console.log('Four animated films');
+    const twoSecNew = await createMovies();
+    console.log(twoSecNew);   
+};
+
+callMovies2();
+
+// async function initMovies() {
+//     let result = null;
+//     //error handling/unit testing try catch block
+//     try {
+//         result = await Promise.all(
+//             createMovies(); // complete first
+//             getMovies(); // complete after createMovies
+//         )
+//         console.log('Success: ', result);
+//     } catch (err) {
+//         console.log('Fail : ', result, err);
+//     }
+// }
+
+// initMovies();
+
+
 
