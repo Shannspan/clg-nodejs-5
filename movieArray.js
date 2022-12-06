31700a1bf7f7e98d3128096f4409ec8c0878b989
[Shann Roberts] <[dpr.1979@bigpond.com]>
2022-12-06 11:53:00 +0800

    create setTimeout and async await

--- movieArray.js
+++ movieArray.js
@@ -1,32 +1,53 @@
 // Iterate through movies list with setTimeout of 1 second enclosing
 
-// must use promise.all to resolve array duration 
-// because promise.all aggregates all results
+const forwardFilmList = [{"Lightyear": "Sci fi Action"}, {"Brave": "Historical drama"}, {"Moana": "Adventure"}];    
 
-async function getMovies(text) {
-    // toupperCase is an inbuilt function
-    return text.toUpperCase();
-}
-// async / await function
-async function callFunction() {
-    const lowerCaseArray = ["Lightyear", "Raya and the Lost Dragon", "Mitchell's vs. the Machines", "Brave"];
-    // .map creates a copy of the array to make new array
-    // await promise.all is required or when you call the callFunction
-    // your return will be promise pending instead of desired array
-    const upperCaseArray = await Promise.all(lowerCaseArray.map( async (movies) => {
-        return await getMovies(movies);
-    }));
-    console.log(upperCaseArray);
+async function getMovies() {
+    return forwardFilmList.reverse();
+
+} 
+// check reverse method is working
+//console.log(getMovies);
+
+function movieTimer() {
+    return new Promise (resolve => {
+    setTimeout(() => {
+    resolve(getMovies());
+    }, 1000);
+})
 }
 
-callFunction();
+// // async / await function
+async function callMovies() {    
+    console.log('Waiting');
+    const oneSecWait = await movieTimer();
+    console.log(oneSecWait);
+}
+callMovies();
 
-// in bash terminal 
-//$ node movieArray.js 
-// returns
-// [
-//   'LIGHTYEAR',
-//   'RAYA AND THE LOST DRAGON',
-//   "MITCHELL'S VS. THE MACHINES",
-//   'BRAVE'
-// ]
+// returns 'Waiting'
+// then after 1 second
+// array in reverse
+
+async function createMovies() {
+    const newMovie = forwardFilmList.push({Mulan: 'Historical Action'});
+    
+} 
+
+
+function movieTimer2() {
+    return new Promise (resolve => {
+    setTimeout(() => {
+    resolve(createMovies());
+    }, 2000);
+})
+}
+
+async function callMovies2() {    
+    
+    console.log(newMovie);
+    const twoSecNew = await movieTimer2();
+    console.log(twoSecNew);
+}
+createMovies();
+
