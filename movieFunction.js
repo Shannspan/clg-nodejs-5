0b82e223406ffd2f79f164bf6851c29ccc613a26
[Shann Roberts] <[dpr.1979@bigpond.com]>
2022-12-09 13:34:08 +0800

    setTimer resolved, need fix function arg object

--- movieFunction.js
+++ movieFunction.js
@@ -1,74 +1,67 @@
-// Iterate through movies list with setTimeout of 1 second enclosing
+// import array from movieArray.js
 
-async function getMovies() {
-    await setTimeout(getMovies, 1000),
-    console.log(forwardFilmList.reverse())
-}
-getMovies();
-// // check reverse method is working
+let movies = require("./movieArray");
 
+// include process module fir argv use later
+const process = require('process');
 
-// function movieTimer() {
-//     return new Promise (resolve => {
-//     setTimeout(() => {
-//     resolve(getMovies());
-//     }, 1000);
-// })
-// }
+// timer 1 seconds
 
-// // // async / await function
-// async function callMovies() {    
-//     console.log('Waiting');
-//     const oneSecWait = await movieTimer();
-//     console.log(oneSecWait);
-// }
-// callMovies();
-
-
-// returns 'Waiting'
-// then after 1 second
-// array in reverse
-
-
-//FIX REQUIRED FOR SECOND SET OF FUNCTIONS AS PUSH NOT APPEARING TO WORK: 
-//$ node movieArray.js 
-// Four animated films
-// 4
-// New movie coming
-
-
-let newFilmList = forwardFilmList.push({"Film": "Mulan", "Genre": "Historical Action"});    
-console.log(newFilmList);
-
-// IDEAS FOR FIX START:
-// REDO ARRAY FORMAT TO BE TITLE: 'TITLE', GENRE: 'GENRE' done
-// RESEARCH .PUSH & WHY NOT WORKING
-// IS EVENT OR EVENTEMMITTER MORE APPROPRIATE?
-// SEPARATE FUNCTIONS TO INDIVIDUAL PAGES done
-// 
-
-
-
-function createMovies() {
-
-    return new Promise (resolve => {        
-    setTimeout(() => {
-    resolve ("New movie coming");
-   
-    
-    }, 2000);
+function resolveAfter1Sec() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 1000);
 });
 }
 
+// timer 2 seconds
+
+function resolveAfter2Sec() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+});
+}
+
+// Iterate through movieArray.js with setTimeout of 1 second enclosing
+
+async function getMovies() {
+    console.log("Reverse movie list will arrive in 1 seconds");
+    // check timer is working by running time of 5000 to make more obvoius - yes
+    await resolveAfter1Sec();
+    // check reverse method is working for iteration - yes
+    console.log(movies.reverse())
+}
+getMovies();
+
+// create a new function that fetches a new argument and pushes to array
+
+//let myArgv = process.argv.slice(2);
+
+//////FIX REQUIRED: CLARIFY ARGUMENT OBJECT IN FUNCTION - I DON'T THINK ARGV VIA CLI IS GOING TO WORK AS PER MATHS BASED ASSIGNMENT
+
+let newTitle = Title: 'Minions The Rise of Gru';
+
+let newStoryline = "Storyline" "The untold story of one 12-year-old’s dream to become the world’s greatest supervillain. In the heart of the 1970s, amidst a flurry of feathered hair and flared jeans, Gru is growing up in the suburbs. A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them.";
+
+const newMovie = newTitle + newStoryline;
+
+console.log(movies.push(newMovie));
 
 
-async function callMovies2() {       
-    console.log('Four animated films');
-    const twoSecNew = await createMovies();
-    console.log(twoSecNew);   
-};
+// async function createMovies() {
+//     await resolveAfter2Sec();
+//     console.log("Adding another movie to the list");
+//     // check timer is working by running time of 5000 to make more obvoius - yes
 
-callMovies2();
+//     // check reverse method is working for iteration - yes
+//     console.log(movies.reverse().push((`${Title: 'string', Storyline: 'String'}`)))
+// }
+// createMovies();
+
+
 
 // async function initMovies() {
 //     let result = null;
